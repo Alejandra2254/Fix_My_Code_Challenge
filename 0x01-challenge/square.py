7e903d5d209935ea37c95dab54d11edb3396ffd5
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-06-05 10:21:56 -0500

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,11 +4,10 @@
 class square():
     """class square"""
     
-    width = 0
-    height = 0
-
-    
     def __init__(self, *args, **kwargs):
+        """Constructor"""
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,15 +15,16 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """permiter of square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Description"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiterOfMySquare())
